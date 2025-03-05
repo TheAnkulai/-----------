224a41c0068d3cc94eabee03abc9c515d0e0325b
TheAnkulai <s79528662968@gmail.com>
2025-03-05 11:40:30 +0300

    adding passive money

--- script/script.js
+++ script/script.js
@@ -1,8 +1,12 @@
 let counter = 0;
 let currentPlusValue = 1;
 let upgradePrice = 50;
+let passivePlus = 0;
+let passiveUpgradePrice = 100;
+let timer;
 const mainButton = document.querySelector('#mainButton');
 const upgradeButton = document.querySelector('#upgrade');
+const passiveUpgradeButton = document.querySelector('#passiveUpgrade');
 
 mainButton.addEventListener('click', function(event) {
     counter += currentPlusValue;
@@ -13,10 +17,33 @@ upgradeButton.addEventListener('click', function(event) {
     if(counter >= upgradePrice) {
         currentPlusValue += 1;
         counter -= upgradePrice;
-        document.getElementById('counter').value = parseFloat(counter.toFixed(2));
+        document.getElementById('counter').value = counter.toFixed(2);
         upgradePrice = (upgradePrice * 1.5).toFixed(2);
         document.getElementById('currentValuePerClick').innerText = `единиц за нажатие: ${currentPlusValue}`;
         document.getElementById('currentUpgradePrice').innerText = `цена улучшения: ${upgradePrice}`;
     }
 })
 
+passiveUpgradeButton.addEventListener('click', function(event) {
+    if(counter >= passiveUpgradePrice) {
+        passivePlus += 0.1;
+        counter -= passiveUpgradePrice;
+        document.getElementById('counter').value = counter.toFixed(2);
+        passiveUpgradePrice = (passiveUpgradePrice * 1.15).toFixed(2);
+        document.getElementById('currentPassivePlus').innerText = `единиц в секунду: ${passivePlus}`;
+        passiveUpgradeButton.innerText = `купить за ${passiveUpgradePrice}`
+        passiveTimer();
+    }
+})
+
+function passiveTimer() {
+    if(timer) {
+        clearInterval(timer);
+    }
+    timer = setInterval(passivePlusometr, 1000);
+}
+
+function passivePlusometr() {
+    counter += passivePlus;
+    document.getElementById('counter').value = counter.toFixed(2);
+}
